9b1d0e95c825d1350fbea716cfa865bf0d95d818
SophieMendez <sophia.mendez.ar@gmail.com>
2023-11-22 23:32:53 -0600

    semana10-tarea
    tarea-semana 10

---
+++ sophia-tarea/tareas10/src/test/java/com/sophietarea/tareas10/Tareas10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophietarea.tareas10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tareas10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
